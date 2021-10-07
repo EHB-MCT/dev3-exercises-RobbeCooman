006f43bb3aec2f582bb262e2a34a7e964cc191f4
RobbeCooman <robbe.cooman@student.ehb.be>
2021-10-07 16:36:33 +0200

    Made rock, paper, scissors exercise

--- session1/src/main/kotlin/Main.kt
+++ session1/src/main/kotlin/Main.kt
@@ -1,11 +1,40 @@
 fun main () {
-    array()
-}
-
-fun array (){
     val array = arrayOf<String>("jef","jos","erik","mark","dirk")
 
-    for (`name` in array) {
-        println("hello: " + `name`)
+    for (name in array) {
+        greet(name)
     }
+    rps()
+}
+
+fun greet (name: String){
+    println("hello $name")
+}
+
+fun rps (){
+
+    //array
+    val choices = arrayOf<String>("rock", "paper", "scissors")
+
+    //computers choice
+    val computerChoice = choices.random()
+
+    //player input
+    print("choose any of the following: rock, paper or scissors?: ")
+    val playerInput = readLine()!!
+
+    //result
+    val result: String
+
+    if (playerInput == computerChoice) result = "tie"
+    else if (playerInput == "rock" && computerChoice == "scissors") result = "you win"
+    else if (playerInput == "paper" && computerChoice == "rock") result = "you win"
+    else if (playerInput == "scissors" && computerChoice == "paper") result = "you win"
+    else if (playerInput == "rock" && computerChoice == "rock") result = "you win"
+    else result = "you lose"
+
+    println("You chose $playerInput and the computer chose $computerChoice, so $result")
+
+
+
 }
