e7094b399a7afaf58148ee69a22c2e86f612d9e5
RobbeCooman <robbe.cooman@student.ehb.be>
2021-10-14 15:18:48 +0200

    Made challenge 1

--- session2/src/main/kotlin/main.kt
+++ session2/src/main/kotlin/main.kt
@@ -1,19 +1,29 @@
 fun main() {
 
-    println("Welkom bij deze leuk game")
-
-
 challengeOne()
-
 
 }
 
 fun challengeOne(){
     //player input
-    print("")
-    val playerInput = readLine()!!
+    println("Welkom bij deze leuk game")
+    println("Can you name one of the big five animals?")
 
+    val answers = setOf<String>("Lion", "Rhino", "Elephant", "Buffalo", "Leopard")
 
+    val playerInput = readLine()
 
+    if (answers.contains(playerInput)){
+        win()
+    }else {
+        gameOver()
+    }
+}
 
+fun win(){
+    println("Congrats")
+}
+
+fun gameOver(){
+    println("you are eaten by a giraffe")
 }
