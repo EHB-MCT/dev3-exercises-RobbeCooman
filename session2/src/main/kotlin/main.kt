ca40c3d129302e62fe63fa83aace0e2e1ff18edf
RobbeCooman <robbe.cooman@student.ehb.be>
2021-10-14 15:09:50 +0200

    Added gitIgnore folder

---
+++ session2/src/main/kotlin/main.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    println("Welkom bij deze leuk game")
+
+
+challengeOne()
+
+
+}
+
+fun challengeOne(){
+    //player input
+    print("")
+    val playerInput = readLine()!!
+
+
+
+
+}
